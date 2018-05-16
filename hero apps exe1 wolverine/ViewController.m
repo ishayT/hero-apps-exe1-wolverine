2ddbce9bb903fded01ef2b0c3599b45046b6e69f
Ishay <ishay@Ishays-Mac.local>
2018-05-16 11:23:37 +0300

    Initial Commit

---
+++ hero apps exe1 wolverine/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hero apps exe1 wolverine
+//
+//  Created by Ishay on 5/16/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
